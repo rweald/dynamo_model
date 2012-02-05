207215f677b2997b2448926afcd081d2c64f0c28
Ryan Weald <ryan@weald.com>
2012-02-05 15:13:32 -0800

    Allow connection creation through config file. This is necessary for migrations and just simplifies connection creation in general

--- lib/dynamo_model/connection.rb
+++ lib/dynamo_model/connection.rb
@@ -1,13 +1,20 @@
-require 'aws/dynamo_db'
-
 module DynamoModel
   class Connection
     def self.create(credentials)
-      @_connection = AWS::DynamoDB.new(
+      _connection = AWS::DynamoDB.new(
         :access_key_id => credentials["access_key"],
         :secret_access_key => credentials["secret_key"]
       )
-      return @_connection
+      return _connection
+    end
+
+    def self.create_from_config
+      config = Configuration.load_configuration.config
+      config = config[DynamoModel.environment]
+      _connection = AWS::DynamoDB.new(
+        :access_key_id => config["access_key"],
+        :secret_access_key => config["secret_key"]
+      )
     end
   end
 end
--- spec/dynamo_model/connection_spec.rb
+++ spec/dynamo_model/connection_spec.rb
@@ -1,14 +1,57 @@
 require 'spec_helper'
-require 'dynamo_model/connection'
+require 'dynamo_model'
 
 describe DynamoModel::Connection do
+  subject { DynamoModel::Connection }
 
   describe ".create" do
     it "should create a connection to AWS DynamoDB" do
       AWS::DynamoDB.should_receive(:new)
-      DynamoModel::Connection.create(:access_key => "hello",
+      subject.create(:access_key => "hello",
                                      :secret_key => "world")
     end
+  end
+
+  describe ".create_from_config" do
+    let(:fake_rails) { double("Fake Rails env object", :env => "production") }
+    let(:fake_configuration) do
+      double("fake config", :config => 
+             {"test" => {
+               "access_key" => "hello", 
+               "secret_key" => "world"
+             },
+              "production" => {
+                 "access_key" => "foo",
+                 "secret_key" => "bar"}
+             })
+    end
+
+    before do
+      DynamoModel::Configuration.stub(:load_configuration => fake_configuration)
+    end
+
+    it "should load the configuration info using the configuration class" do
+      DynamoModel::Configuration.should_receive(:load_configuration)
+      subject.create_from_config
+    end
+
+    it "should use the dynamo env if rails env is not defined" do
+      AWS::DynamoDB.should_receive(:new) do |credentials|
+        credentials[:access_key_id].should == "hello"
+        credentials[:secret_access_key].should == "world"
+      end
+      subject.create_from_config
+    end
+
+    it "should use rails env to access specific config if Rails is available" do
+      Rails = fake_rails
+      AWS::DynamoDB.should_receive(:new) do |credentials|
+        credentials[:access_key_id].should == "foo"
+        credentials[:secret_access_key].should == "bar"
+      end
+      subject.create_from_config
+    end
+
   end
 
 end
