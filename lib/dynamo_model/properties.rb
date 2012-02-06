e1df8cb77b77d73a433b1d6c90569ee78a978885
Ryan Weald <ryan@weald.com>
2012-02-05 21:41:14 -0800

    making some progress with the basic wiring but still have work to do.

--- lib/dynamo_model/properties.rb
+++ lib/dynamo_model/properties.rb
@@ -5,23 +5,53 @@ require 'active_model/attribute_methods'
 
 module DynamoModel
 
-  #module Properties
-    #extend ActiveSupport::Concern
+  module Properties
+    extend ActiveSupport::Concern
+    include ActiveModel::AttributeMethods
 
-    #included do
-      #include ClassMethods
-    #end
+    included do
+      include ClassMethods
+    end
 
-    #module ClassMethods
-      #def property(name, options = {})
-        #value = options[:default] || nil
-        #properties[name] = options[:default] || nil
-      #end
+    module ClassMethods
+      def property(name, options = {})
+        undefine_attribute_methods
+        properties[name] = DynamoModel::Property.new(name, options.delete(:default), options)
+      end
 
-      #def properties
-        #@_properties || {}.with_indifferent_access
-      #end
-    #end
-  #end
+      def properties
+        @properties || {}.with_indifferent_access
+      end
+
+      def define_attribute_methods
+        super(properties.keys)
+      end
+    end
+
+    def attributes
+      raw_attributes.reject { |k, v| !respond_to?(k) }
+    end
+
+    def raw_attributes
+      @attributes = @attributes || {}
+      self.class.properties.values.inject(@attributes.with_indifferent_access) do |hash, prop|
+        hash[prop.key] = attribute(prop.key)
+        hash
+      end
+    end
+
+
+    # @private
+    def method_missing(method, *args, &block)
+      self.class.define_attribute_methods
+      super
+    end
+
+    # @private
+    def respond_to?(*args)
+      self.class.define_attribute_methods
+      super
+    end
+  end
 
 end
