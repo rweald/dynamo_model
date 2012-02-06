d58f33a72da3df7bbd859cdef6089616208ff3df
Ryan Weald <ryan@weald.com>
2012-02-06 08:37:05 -0800

    Merge adding_dynamic_attributes using squash commit. Squashed commit of the following:
    commit f4a883efe142f65f4423abf6ae86524a67be84db
    Author: Ryan Weald <ryan@weald.com>
    Date:   Mon Feb 6 08:35:43 2012 -0800
    
        Attributes can now be defined dynamically using property method.
    
    commit 24556fe935cbced417db9f2d64aa7996b2a8f14f
    Author: Ryan Weald <ryan@weald.com>
    Date:   Sun Feb 5 21:42:39 2012 -0800
    
        travis only displays status of master branch.
    
    commit e1df8cb77b77d73a433b1d6c90569ee78a978885
    Author: Ryan Weald <ryan@weald.com>
    Date:   Sun Feb 5 21:41:14 2012 -0800
    
        making some progress with the basic wiring but still have work to do.

--- lib/dynamo_model/properties.rb
+++ lib/dynamo_model/properties.rb
@@ -5,23 +5,74 @@ require 'active_model/attribute_methods'
 
 module DynamoModel
 
-  #module Properties
-    #extend ActiveSupport::Concern
+  module Properties
+    extend ActiveSupport::Concern
+    include ActiveModel::AttributeMethods
 
-    #included do
-      #include ClassMethods
-    #end
+    included do
+      include ClassMethods
+      attribute_method_suffix "="
+    end
 
-    #module ClassMethods
-      #def property(name, options = {})
-        #value = options[:default] || nil
-        #properties[name] = options[:default] || nil
-      #end
+    module ClassMethods
+      def property(name, options = {})
+        undefine_attribute_methods
+        properties[name] = DynamoModel::Property.new(name, options.delete(:default), options)
+      end
 
-      #def properties
-        #@_properties || {}.with_indifferent_access
-      #end
-    #end
-  #end
+      def properties
+        @properties = @properties || {}.with_indifferent_access
+      end
+
+      def define_attribute_methods
+        super(properties.keys)
+      end
+    end
+
+    def attributes
+      @attributes || raw_attributes
+    end
+
+    def raw_attributes
+      @attributes = self.class.properties.values.inject({}) do |hash, prop|
+        hash[prop.key] = prop.default_value
+        hash
+      end.with_indifferent_access
+    end
+
+    def [](attr_name)
+      attribute(attr_name)
+    end
+
+    def []=(attr_name, value)
+      self.send(:attribute=, attr_name, value)
+    end
+
+    def attribute(attr_name)
+      attributes[attr_name]
+    end
+
+    def attribute=(attr_name, value)
+      if prop = self.class.properties[attr_name]
+        @attributes[attr_name] = prop.convert_value(value)
+      else
+        raise "Invalid Property Value"
+      end
+    end
+
+    def attribute_method?(attr_name)
+      self.class.properties.include?(attr_name)
+    end
+    
+    def method_missing(method, *args, &block)
+      self.class.define_attribute_methods
+      super
+    end
+
+    def respond_to?(*args)
+      self.class.define_attribute_methods
+      super
+    end
+  end
 
 end
--- spec/dynamo_model/properties_spec.rb
+++ spec/dynamo_model/properties_spec.rb
@@ -1,28 +1,28 @@
 require 'spec_helper'
 
-#describe DynamoModel::Properties do
-  #class Tweet
-    #include DynamoModel::Properties
+describe DynamoModel::Properties do
+  class Tweet
+    include DynamoModel::Properties
     
-    #property :content, String
-    #property :posted_at, DateTime, :default => DateTime.now
-  #end
+    property :content
+    property :posted_at, :default => DateTime.now, :conversion => proc { |v| v.to_s }
+  end
 
-  #describe ".property" do
-    #let(:tweet) { Tweet.new }
+  describe ".property" do
+    let(:tweet) { Tweet.new }
 
-    #it "should create a getter with the name provided to property call" do
-      #tweet.should respond_to(:content)
-    #end
+    it "should create a getter with the name provided to property call" do
+      tweet.should respond_to(:content)
+    end
 
-    #it "should create a setter for the given property name" do
-      #tweet.should respond_to(:content=)
-    #end
+    it "should create a setter for the given property name" do
+      tweet.should respond_to(:content=)
+    end
 
-    #it "should return the default value if a normal value has not been set" do
-      #tweet.posted_at.to_date.should == DateTime.today.to_date
-    #end
+    it "should return the default value if a normal value has not been set" do
+      tweet.posted_at.should == DateTime.now.to_s
+    end
     
-  #end
+  end
   
-#end
+end
